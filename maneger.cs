db5db64a950c4bd068ffc4efec93420fb70f709d
IgorBondar16 <igorbondar806@gmail.com>
2023-09-23 18:41:53 +0300

    Created class manager

---
+++ maneger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamPlayEx
+{
+    public class Maneger : User
+    {
+        public Maneger(string name, string password, string team, string role, string email, string phone) : base(name, password, team, role, email, phone)
+        {
+
+        }
+    }
+}
